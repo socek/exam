b411cd64b194d975977a97330b727d4f65e2e584
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-01-20 22:30:32 +0100

    Fixed tests for Answer.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,6 @@
 class Answer < ActiveRecord::Base
   validates :content, presence: true
   validates :right,
-      presence: true,
       inclusion: { in: [true, false] }
 
   belongs_to :question
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,9 +4,9 @@ describe Answer do
   it { should validate_presence_of :content }
 
   it "should have #right with type of bool" do
-    FactoryGirl.create(:answer).should be_valid
-    FactoryGirl.create(:answer, right: true).should be_valid
-    FactoryGirl.create(:answer, right: "this is string").should_not be_valid
+    FactoryGirl.build(:answer).should be_valid
+    FactoryGirl.build(:answer, right: true).should be_valid
+    FactoryGirl.build(:answer, right: nil).should_not be_valid
   end
 
   it { should belong_to :question }
