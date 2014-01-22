e063f768d9d87ab11332faa0dcc13149cda0731f
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-01-22 21:33:58 +0100

    Mailer fix.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,11 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def welcome_email(user)
+  def score_email(user, score, max_score)
       @user = user
+      @score = score
+      @max_score = max_score
       @url  = 'http://example.com/login'
-      mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+      mail(to: @user.email, subject: 'You score result!')
     end
 end
